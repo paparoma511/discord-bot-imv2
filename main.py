3d7d0d2580e0b0ba657339410f5c7a5b0c564728
paparoma511 <paparoma511@gmail.com>
2026-06-16 21:38:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -159,7 +159,7 @@ class ApplicationForm(discord.ui.Modal):
                 overwrites[role] = discord.PermissionOverwrite(read_messages=True)
 
         channel = await guild.create_text_channel(
-            name=f"заявка-{member.name}",
+            name=f"【⚠️】заявка-{member.name}",
             category=category,
             overwrites=overwrites
         )
@@ -172,7 +172,6 @@ class ApplicationForm(discord.ui.Modal):
         embed.add_field(name="О себе", value=self.about.value, inline=False)
 
         await channel.send(embed=embed, view=AdminView(member.id))
-        await channel.send(view=CloseView(member.id))
 
         await log(f"Новая заявка: {member}")
 
@@ -195,7 +194,7 @@ class ReportForm(discord.ui.Modal):
         category = discord.utils.get(guild.categories, id=CATEGORY_REPORTS)
 
         channel = await guild.create_text_channel(
-            name=f"жалоба-{member.name}",
+            name=f"【⚠️】жалоба-{member.name}",
             category=category
         )
 
@@ -212,15 +211,26 @@ class ReportForm(discord.ui.Modal):
 
 # ================= MAIN PANEL =================
 
-class MainView(discord.ui.View):
+class ApplicationView(discord.ui.View):
     def __init__(self):
         super().__init__(timeout=None)
 
-    @discord.ui.button(label="Подать заявку в администрацию", style=discord.ButtonStyle.success)
+    @discord.ui.button(
+        label="Подать заявку в администрацию",
+        style=discord.ButtonStyle.success
+    )
     async def app(self, interaction: discord.Interaction, button: discord.ui.Button):
         await interaction.response.send_modal(ApplicationForm())
 
-    @discord.ui.button(label="Пожаловаться на игрока/администратора", style=discord.ButtonStyle.danger)
+
+class ReportView(discord.ui.View):
+    def __init__(self):
+        super().__init__(timeout=None)
+
+    @discord.ui.button(
+        label="Пожаловаться на игрока/администратора",
+        style=discord.ButtonStyle.danger
+    )
     async def rep(self, interaction: discord.Interaction, button: discord.ui.Button):
         await interaction.response.send_modal(ReportForm())
 
@@ -242,6 +252,70 @@ async def on_ready():
         print(f"SYNC ERROR: {e}")
 
     print(f"BOT ONLINE: {bot.user}")
+
+@bot.tree.command(
+    name="application",
+    description="Отправить панель подачи заявки"
+)
+async def application(interaction: discord.Interaction):
+
+    if not is_admin(interaction.user):
+        return await interaction.response.send_message(
+            "❌ Нет прав",
+            ephemeral=True
+        )
+
+    embed = discord.Embed(
+        title="Подача заявки в администрацию",
+        description=(
+            "Условия при которых ваша заявка будет одобрена:\n"
+            "• вам должно быть не меньше 13 лет\n"
+            "• вы должны знать правила сервера\n"
+            "• у вас должно быть наигранно не менее 50 часов в SCP:SL\n"
+            "• у вас должен быть открытый Steam аккаунт\n"
+            "-----------------------------\n"
+            "Без этого ваша заявка будет автоматически отклонена\n"
+            "⚠️ Шуточные заявки будут отклоняться. Кто подавал шуточную заявку получит наказание на усмотрение высшей администрации ⚠️"
+        )
+    )
+
+    await interaction.channel.send(
+        embed=embed,
+        view=ApplicationView()
+    )
+
+    await interaction.response.send_message(
+        "✅ Панель заявок отправлена",
+        ephemeral=True
+    )
+
+
+@bot.tree.command(
+    name="reportplayer",
+    description="Отправить панель жалоб"
+)
+async def reportplayer(interaction: discord.Interaction):
+
+    if not is_admin(interaction.user):
+        return await interaction.response.send_message(
+            "❌ Нет прав",
+            ephemeral=True
+        )
+
+    embed = discord.Embed(
+        title="Подача жалобы",
+        description="Нажмите кнопку ниже для подачи жалобы."
+    )
+
+    await interaction.channel.send(
+        embed=embed,
+        view=ReportView()
+    )
+
+    await interaction.response.send_message(
+        "✅ Панель жалоб отправлена",
+        ephemeral=True
+    )
 
 
 # ================= RUN =================
