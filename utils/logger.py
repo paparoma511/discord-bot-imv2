e75ba8ccb8a0786ce4b8855403a438b82a889344
paparoma511 <paparoma511@gmail.com>
2026-07-08 03:32:49 +0300

    Update logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -1,1 +1,21 @@
-.
+from config import LOG_CHANNEL_ID
+
+
+
+async def log(bot, text):
+
+    try:
+
+        channel = await bot.fetch_channel(
+            LOG_CHANNEL_ID
+        )
+
+
+        await channel.send(
+            f"ℹ️ **LOGGER**\n{text}"
+        )
+
+
+    except Exception:
+
+        pass
