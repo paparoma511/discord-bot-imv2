16737d00609293f630072b137a9f28c59fd0c2ad
paparoma511 <paparoma511@gmail.com>
2026-07-08 03:36:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from discord.ext import commands
 
 from dotenv import load_dotenv
 
+from database.database import create_database
+
 
 from views.application_buttons import ApplicationView
 
@@ -97,11 +99,11 @@ async def main():
 
     async with bot:
 
-        await load_extensions()
 
-        await bot.start(
-            TOKEN
-        )
+        await create_database()
+
+
+        await load_extensions()
 
 
 
