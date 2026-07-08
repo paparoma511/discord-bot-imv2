656fb73aeb5153d8ca5c9c6d4249a71f8dabe1b3
paparoma511 <paparoma511@gmail.com>
2026-07-08 03:37:04 +0300

    Update application_buttons.py

--- views/application_buttons.py
+++ views/application_buttons.py
@@ -1,1 +1,106 @@
-.
+import discord
+
+from modals.application_form import DepartmentForm
+
+
+
+class ApplicationView(discord.ui.View):
+
+    def __init__(self):
+
+        super().__init__(
+            timeout=None
+        )
+
+
+
+    @discord.ui.button(
+
+        label="NORULES",
+
+        style=discord.ButtonStyle.primary,
+
+        custom_id="application_norules"
+
+    )
+
+    async def norules(
+
+        self,
+
+        interaction: discord.Interaction,
+
+        button: discord.ui.Button
+
+    ):
+
+
+        await interaction.response.send_modal(
+
+            DepartmentForm(
+                "NORULES"
+            )
+
+        )
+
+
+
+    @discord.ui.button(
+
+        label="EVENTS",
+
+        style=discord.ButtonStyle.success,
+
+        custom_id="application_events"
+
+    )
+
+    async def events(
+
+        self,
+
+        interaction: discord.Interaction,
+
+        button: discord.ui.Button
+
+    ):
+
+
+        await interaction.response.send_modal(
+
+            DepartmentForm(
+                "EVENTS"
+            )
+
+        )
+
+
+
+    @discord.ui.button(
+
+        label="DISCORD",
+
+        style=discord.ButtonStyle.secondary,
+
+        custom_id="application_discord"
+
+    )
+
+    async def discord_department(
+
+        self,
+
+        interaction: discord.Interaction,
+
+        button: discord.ui.Button
+
+    ):
+
+
+        await interaction.response.send_modal(
+
+            DepartmentForm(
+                "DISCORD"
+            )
+
+        )
