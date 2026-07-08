feacca1b95822fcadbeb64728613456abd997e36
paparoma511 <paparoma511@gmail.com>
2026-07-08 03:32:34 +0300

    Update permissions.py

--- utils/permissions.py
+++ utils/permissions.py
@@ -1,1 +1,15 @@
-.
+import discord
+
+from config import ADMIN_ROLE_IDS
+
+
+
+def is_admin(member: discord.Member):
+
+    return any(
+
+        role.id in ADMIN_ROLE_IDS
+
+        for role in member.roles
+
+    )
